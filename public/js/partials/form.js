7794acfe35c8b7a2db7d9cf6e41d76dabd728962
Brenton Cozby <brenton.cozby@gmail.com>
2017-01-15 16:17:13 -0500

    complete form and begin adding form functionality

--- public/js/partials/form.js
+++ public/js/partials/form.js
@@ -1,5 +1,55 @@
-module.exports = {
+const $apiUrl = $('#api-url');
+const $options = $('#search')
+    .add('#dateRestrictNum')
+    .add('#dateRestrictType')
+    .add('input[name=imgSize]')
+    .add('input[name=imgType]')
+    .add('input[name=imgDominantColor]')
+    .add('input[name=imgColorType]')
+    .add('input[name=safe]');
+
+const optionSelectors = [
+    ['#search', 'search'],
+    ['input[name=imgSize]:checked', 'imgSize'],
+    ['input[name=imgType]:checked', 'imgType'],
+    ['input[name=imgDominantColor]:checked', 'imgDominantColor'],
+    ['input[name=imgColorType]:checked', 'imgColorType'],
+    ['input[name=safe]:checked', 'safe']
+];
+
+const formMethods = {
     init: function() {
         $('select').material_select();
+        $options.on('change', formMethods.buildUrl);
+        $('#reset').click(formMethods.resetForm);
+        $('#get-images').click(formMethods.getImages);
+    },
+    buildUrl: function() {
+        var urlHTML = 'http://image-search-fcc-cozby.herokuapp.com/api?';
+        const drNum = $('#dateRestrictNum').val();
+        const drType = $('#dateRestrictType').val();
+        optionSelectors.forEach(selector => {
+            var value = $(selector[0]).val();
+            var param = selector[1];
+            if(value) {
+                urlHTML += `${param}=${value}&`;
+            }
+        });
+        if(drNum && drType) {
+            urlHTML += `dateRestrict=${drType}[${drNum}]&`;
+        }
+        $apiUrl.html(urlHTML + 'start=0');
+        $apiUrl.attr('href', urlHTML + 'start=0');
+    },
+    resetForm: function() {
+        $('#search').val('');
+        $('#dateRestrictNum').val('');
+        $('input').attr('checked', false);
+        $apiUrl.html('http://image-search-fcc-cozby.herokuapp.com/api?search=grumpycat&start=0');
+    },
+    getImages: function() {
+
     }
 };
+
+module.exports = formMethods;
