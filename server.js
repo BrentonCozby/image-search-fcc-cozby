7294db79baf0254db0df5a6f47643354b97cf6fa
Brenton Cozby <brenton.cozby@gmail.com>
2017-01-15 09:27:43 -0500

    add Google's Materialize css framework

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.set('port', (process.env.PORT || 3000));
 app.set('views', path.join(__dirname, 'views', 'pages'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // set routes
 [
     require('./app/routes/index.js'),
